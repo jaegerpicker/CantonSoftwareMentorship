9d1118e379cc39eeded593c5e43bf2dab031f56c
Shawn Campbell <jaegerpicker@gmail.com>
2013-06-02 13:23:03 -0400

    Adding C# and Ruby
    Adding examples in C# and Ruby. I'll add Java and Closure next. Then go back and add some unit tests. Might also add C and C++ versions.
    
    Signed-off-by: Shawn Campbell <jaegerpicker@gmail.com>

---
+++ ShawnCampbell-Month1-CSharp-StringSearching/StringSearching/StringSearching/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+using System.Collections.Generic;
+namespace StringSearching
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader reader = File.OpenText(args[0]))
+                while (!reader.EndOfStream)
+                {
+                    string line = reader.ReadLine();
+                    if (null == line)
+                        continue;
+                    // do something with line
+                }
+        }
+    }
+}
