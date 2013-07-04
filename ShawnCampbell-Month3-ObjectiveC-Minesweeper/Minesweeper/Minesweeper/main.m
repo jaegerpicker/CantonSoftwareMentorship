a576c09f7e1d90eef20053f912bb8fafe123375a
Shawn Campbell <jaegerpicker@gmail.com>
2013-07-03 23:54:48 -0400

    Started Objective-C example

---
+++ ShawnCampbell-Month3-ObjectiveC-Minesweeper/Minesweeper/Minesweeper/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Minesweeper
+//
+//  Created by Shawn Campbell on 7/3/13.
+//  Copyright (c) 2013 Shawn Campbell. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
