a6798c78b5472fb9496a27161ae29a100122c3bc
Shawn Campbell <jaegerpicker@gmail.com>
2013-07-10 22:33:00 -0400

    added the command line interface that reads in the file

--- ShawnCampbell-Month3-ObjectiveC-Minesweeper/Minesweeper/Minesweeper/main.m
+++ ShawnCampbell-Month3-ObjectiveC-Minesweeper/Minesweeper/Minesweeper/main.m
@@ -7,6 +7,8 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "MineSweeperMatrixParser.h"
+#import "MineSweeperField.h"
 
 int main(int argc, const char * argv[])
 {
@@ -14,7 +16,28 @@ int main(int argc, const char * argv[])
     @autoreleasepool {
         
         // insert code here...
-        NSLog(@"Hello, World!");
+        NSArray *arguements = [[NSProcessInfo processInfo] arguments];
+        //NSString *workingDir = [[[NSProcessInfo processInfo] environment] objectForKey:@"PWD"];
+        //NSLog(@"working directory = %@", workingDir);
+        NSStringEncoding usedEncoding = 0;
+        NSError *err = nil;
+        NSString *path = [[NSString alloc] initWithContentsOfFile:arguements[1] encoding:usedEncoding error:&err];
+        //NSLog(@"file: %@", path);
+        NSArray *lines = [[NSArray alloc] init];
+        lines = [path componentsSeparatedByString:@"\n"];
+        for(NSString *s in lines) {
+            if([s length] == 0) {
+                break;
+            }
+            MineSweeperMatrixParser *m = [[MineSweeperMatrixParser alloc] init];
+            //NSString* line = @"4,4;*........*......";
+            BOOL ret = [m parseInput:s];
+            if(ret){
+                MineSweeperField* f = [[MineSweeperField alloc] init:m.matrix];
+                NSString* output = [f solve];
+                NSLog(@"%@", output);
+            }
+        }
         
     }
     return 0;
