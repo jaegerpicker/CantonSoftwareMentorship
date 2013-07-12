d9ca4bcc4384a53f1d9882b6fec71dcc6a3e651f
Shawn Campbell <jaegerpicker@gmail.com>
2013-07-11 23:49:16 -0400

    Playing around with clojure may try and make a solution with it, may not :)

---
+++ ShawnCampbell-Month3-Clojure-Minesweeper/minesweeper/src/minesweeper/core.clj
@@ -0,0 +1,1 @@
+(ns minesweeper.core)
---
+++ ShawnCampbell-Month3-Clojure-Minesweeper/minesweeper/test/minesweeper/test/core.clj
@@ -0,0 +1,6 @@
+(ns minesweeper.test.core
+  (:use [minesweeper.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
