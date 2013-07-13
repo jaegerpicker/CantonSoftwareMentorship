9b7bbed353002d96d74df0b9e7d0123103cd24f4
Shawn Campbell <jaegerpicker@gmail.com>
2013-07-13 00:00:36 -0400

    Ugh.. Brian hurts lisp hard... got the file reading and printing each line. enough for tonight I think.

--- ShawnCampbell-Month3-Clojure-Minesweeper/minesweeper/project.clj
+++ ShawnCampbell-Month3-Clojure-Minesweeper/minesweeper/project.clj
@@ -1,3 +1,5 @@
 (defproject minesweeper "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.3.0"]])
+  :description "Minesweeper test solution"
+  :dependencies [[org.clojure/clojure "1.3.0"]]
+  :aot [minesweeper.core]
+  :main [minesweeper.core])
--- ShawnCampbell-Month3-Clojure-Minesweeper/minesweeper/src/minesweeper/core.clj
+++ ShawnCampbell-Month3-Clojure-Minesweeper/minesweeper/src/minesweeper/core.clj
@@ -1,1 +1,13 @@
-(ns minesweeper.core)
+(ns minesweeper.core (:require [clojure.java.io :as io]) )
+
+(def *matrix* (ref []))
+
+(def parseMatrix (fn [mstring]
+                   ))
+
+(def solve (fn [line]
+             (println line)))
+
+(with-open [rdr (io/reader
+                  "/Users/shawn/Documents/CantonSoftwareMentorship/ShawnCampbell-Month3-Clojure-Minesweeper/minesweeper/src/minesweeper/input.txt")]
+  (doseq [line (line-seq rdr)] (solve line)))
