923629c167c8835db133ea8f880b453b6756f46b
Shawn Campbell <jaegerpicker@gmail.com>
2013-07-15 01:07:50 -0400

    Worked on clojure example. It's parsing the rows of the matrix now. About half way done. Completely different way of thinking about code, it's been challenging and fun but hard and fustrating too.

--- ShawnCampbell-Month3-Clojure-Minesweeper/minesweeper/project.clj
+++ ShawnCampbell-Month3-Clojure-Minesweeper/minesweeper/project.clj
@@ -1,5 +1,5 @@
 (defproject minesweeper "1.0.0-SNAPSHOT"
   :description "Minesweeper test solution"
   :dependencies [[org.clojure/clojure "1.3.0"]]
-  :aot [minesweeper.core]
-  :main [minesweeper.core])
+  :aot minesweeper.core
+  :main minesweeper.core)
--- ShawnCampbell-Month3-Clojure-Minesweeper/minesweeper/src/minesweeper/core.clj
+++ ShawnCampbell-Month3-Clojure-Minesweeper/minesweeper/src/minesweeper/core.clj
@@ -1,13 +1,36 @@
-(ns minesweeper.core (:require [clojure.java.io :as io]) )
+(ns minesweeper.core (:require [clojure.java.io :as io][clojure.string :as cstr :only (split)]) (:gen-class))
 
-(def *matrix* (ref []))
+(def tokenedLine (fn [line]
+	(cstr/split line #"")))
+
+(defn parse-int [s]
+   (Integer. (re-find  #"\d+" s )))
+
+(def size (fn [tLine]
+	[(parse-int (nth tLine 1)) (parse-int (nth tLine 3))]))
+
+(defn row [vLine rowNum vsize]
+	(drop (* rowNum (nth vsize 1)) (take (nth vsize 1) vLine)))
+
 
 (def parseMatrix (fn [mstring]
+                   (let [this-tLine (tokenedLine mstring)
+                   		this-size (size (tokenedLine mstring))]
+                   (println (row 
+                   		(drop 5 (take 
+                   			(- (count this-tLine) 4) 
+                   			this-tLine))
+                   		0
+                   		this-size
+                   	)
+                   )
+                   ;(println this-size)
+                   )
                    ))
 
 (def solve (fn [line]
-             (println line)))
+             (parseMatrix line)))
 
-(with-open [rdr (io/reader
+(def -main (fn [] (with-open [rdr (io/reader
                   "/Users/shawn/Documents/CantonSoftwareMentorship/ShawnCampbell-Month3-Clojure-Minesweeper/minesweeper/src/minesweeper/input.txt")]
-  (doseq [line (line-seq rdr)] (solve line)))
+  (doseq [line (line-seq rdr)] (solve line)))))
