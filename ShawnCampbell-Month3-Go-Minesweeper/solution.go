51ecdcd2fff74f8efa8e91ee8b5549253e28928e
Shawn Campbell <jaegerpicker@gmail.com>
2013-07-20 00:42:36 -0400

    Worked on go and python examples

--- ShawnCampbell-Month3-Go-Minesweeper/solution.go
+++ ShawnCampbell-Month3-Go-Minesweeper/solution.go
@@ -1,2 +1,52 @@
 package minesweeper
 
+import (
+	"os"
+	"bufio"
+	"bytes"
+	"io"
+	"fmt"
+	"strings"
+)
+
+func readLines(path string) (lines []string, err error) {
+	var (
+		file *os.file
+		part []byte
+		prefix bool
+		)
+	if file, err = os.Open(path); err != nil {
+		return
+	}
+	defer file.close()
+
+	reader := bufio.NewReader(file)
+	buffer := bytes.NewBuffer(make([]byte, 0))
+	for {
+		if part, prefix, err = reader.ReadLine(); err != nil {
+			break
+		}
+		buffer.Write(part)
+		if !prefix {
+			lines = append(lines, buffer.String())
+			buffer.Reset()
+		}
+	}
+	if err == io.EOF {
+		err = nil
+	}
+	return
+}
+
+
+func main() {
+	lines, err = readLines("input.txt")
+	if err != nil {
+		fmt.Println("Error: %s\n", err)
+		return
+	}
+	for -, line := range lines {
+		fmt.Println(line)
+	}
+	
+}
--- ShawnCampbell-Month3-Python-Minesweeper/solution.py
+++ ShawnCampbell-Month3-Python-Minesweeper/solution.py
@@ -1,1 +1,6 @@
 import sys
+test_cases = open(sys.argv[1], 'r')
+for test in test_cases:
+	if test != '':
+		pass
+	pass
