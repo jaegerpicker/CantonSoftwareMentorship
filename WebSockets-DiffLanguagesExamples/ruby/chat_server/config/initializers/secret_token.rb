78f1e4cc5ab8fb80d760bfc436b017c1a5147e9f
Shawn Campbell <scampbell@cashstar.com>
2013-07-25 13:17:17 -0400

    Added ruby

---
+++ WebSockets-DiffLanguagesExamples/ruby/chat_server/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ChatServer::Application.config.secret_key_base = '8995194577d56de2d4ac3ac1d9e0a2b64c77244ed5e6c87e97e5d552db2e69489a3fab8e9e3b9462ad7b07150c8ca55ddd94ac9c78bc177f6c35587af1d5d847'
