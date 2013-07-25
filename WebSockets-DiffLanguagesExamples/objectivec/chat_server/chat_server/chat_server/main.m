4dc8b07138ad031e3668a531c04b063e2af84bca
Shawn Campbell <scampbell@cashstar.com>
2013-07-25 10:44:28 -0400

    Working on the websockets examples

---
+++ WebSockets-DiffLanguagesExamples/objectivec/chat_server/chat_server/chat_server/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  chat_server
+//
+//  Created by Shawn Campbell on 7/25/13.
+//  Copyright (c) 2013 Shawn Campbell. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
