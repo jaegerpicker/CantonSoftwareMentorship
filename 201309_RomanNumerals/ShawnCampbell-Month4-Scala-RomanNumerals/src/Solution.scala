911a768bf0bb73d0c4ee47306cc334cc7f4abb65
Shawn Campbell <jaegerpicker@gmail.com>
2013-08-14 00:30:03 -0400

    Scala Init

---
+++ 201309_RomanNumerals/ShawnCampbell-Month4-Scala-RomanNumerals/src/Solution.scala
@@ -0,0 +1,10 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: shawn
+ * Date: 8/14/13
+ * Time: 12:29 AM
+ * To change this template use File | Settings | File Templates.
+ */
+class Solution {
+
+}
