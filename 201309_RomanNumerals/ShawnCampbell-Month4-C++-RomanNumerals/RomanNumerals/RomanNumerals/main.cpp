7125c28c9cbefa2e07ecfe50180eacfb08656bec
Shawn Campbell <jaegerpicker@gmail.com>
2013-08-13 21:20:00 -0400

    Finished up C++ example

--- 201309_RomanNumerals/ShawnCampbell-Month4-C++-RomanNumerals/RomanNumerals/RomanNumerals/main.cpp
+++ 201309_RomanNumerals/ShawnCampbell-Month4-C++-RomanNumerals/RomanNumerals/RomanNumerals/main.cpp
@@ -15,10 +15,21 @@ int main(int argc, const char * argv[])
 {
     string line;
     ifstream myfile (argv[1]);
+    int nums [13] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
+    string numerals [13] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+    string results = "";
     if (myfile.is_open()) {
         while (myfile.good()) {
             getline(myfile, line);
-            cout << line << endl;
+            int number = atoi(line.c_str());
+            results = "";
+            for(int i = 0; i < 13; i++) {
+                while (number >= nums[i]) {
+                    number -= nums[i];
+                    results += numerals[i];
+                }
+            }
+            cout << results << endl;
         }
         myfile.close();
     }
