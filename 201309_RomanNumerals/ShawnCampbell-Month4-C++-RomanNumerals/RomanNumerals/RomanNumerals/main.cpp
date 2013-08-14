b2467ba665334a1c06b809953c2e4e8ca5f40851
Shawn Campbell <jaegerpicker@gmail.com>
2013-08-13 22:02:47 -0400

    Added benchmark info

--- 201309_RomanNumerals/ShawnCampbell-Month4-C++-RomanNumerals/RomanNumerals/RomanNumerals/main.cpp
+++ 201309_RomanNumerals/ShawnCampbell-Month4-C++-RomanNumerals/RomanNumerals/RomanNumerals/main.cpp
@@ -9,35 +9,58 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <ctime>
+#include <chrono>
 using namespace std;
 
-int main(int argc, const char * argv[])
-{
-    string line;
-    ifstream myfile (argv[1]);
-    int nums [13] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
-    string numerals [13] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
-    string results = "";
-    if (myfile.is_open()) {
-        while (myfile.good()) {
-            getline(myfile, line);
-            int number = atoi(line.c_str());
-            results = "";
-            for(int i = 0; i < 13; i++) {
-                while (number >= nums[i]) {
-                    number -= nums[i];
-                    results += numerals[i];
-                }
-            }
-            cout << results << endl;
+int write_test_file(string file_name) {
+    ofstream test_file(file_name);
+    if(test_file.is_open()) {
+        for(int i = 0; i < 10000000; i++) {
+            test_file << rand() % 3999 + 1 << endl;
         }
-        myfile.close();
+        test_file.close();
     }
-    else
-    {
-        cout << "Unable to open file\n";
-    }
-    
     return 0;
 }
 
+
+
+int main(int argc, const char * argv[])
+{
+    const char * file_gen = "f";
+    cout << argv[2] << " " << file_gen << endl;
+    if(string(argv[2]) == "f") {
+        write_test_file(argv[1]);
+    } else {
+        auto start = std::chrono::steady_clock::now();
+        string line;
+        ifstream myfile (argv[1]);
+        int nums [13] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
+        string numerals [13] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        string results = "";
+        if (myfile.is_open()) {
+            while (myfile.good()) {
+                getline(myfile, line);
+                int number = atoi(line.c_str());
+                results = "";
+                for(int i = 0; i < 13; i++) {
+                    while (number >= nums[i]) {
+                        number -= nums[i];
+                        results += numerals[i];
+                    }
+                }
+                cout << results << endl;
+            }
+            myfile.close();
+        }
+        else
+        {
+            cout << "Unable to open file\n";
+        }
+        auto end = std::chrono::steady_clock::now();
+        auto elapsed = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+        cout << "It took me " << elapsed.count() << " microseconds." << endl;
+    }
+    return 0;
+}
