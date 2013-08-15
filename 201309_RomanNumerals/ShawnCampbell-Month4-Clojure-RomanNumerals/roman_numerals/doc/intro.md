ae353b08d67d0a41d6a16c7ead4cd77b3f677541
Shawn Campbell <jaegerpicker@gmail.com>
2013-08-15 11:36:00 -0400

    Cleaned out some unused code

--- 201309_RomanNumerals/ShawnCampbell-Month4-Clojure-RomanNumerals/roman_numerals/doc/intro.md
+++
@@ -1,3 +0,0 @@
-# Introduction to roman_numerals
-
-TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
--- 201309_RomanNumerals/ShawnCampbell-Month4-Clojure-RomanNumerals/roman_numerals/test/roman_numerals/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns roman-numerals.core-test
-  (:require [clojure.test :refer :all]
-            [roman-numerals.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
