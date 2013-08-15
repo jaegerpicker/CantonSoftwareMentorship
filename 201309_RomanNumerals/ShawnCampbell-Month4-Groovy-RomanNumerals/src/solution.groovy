8841f22e8ba826bd4d4a2113e2bdc903fa9bebbd
Shawn Campbell <jaegerpicker@gmail.com>
2013-08-15 10:23:39 -0400

    Added Groovy, but not using it for benchmarks. It's slow and really annoying for non grails type of apps

--- 201309_RomanNumerals/ShawnCampbell-Month4-Groovy-RomanNumerals/src/solution.groovy
+++ 201309_RomanNumerals/ShawnCampbell-Month4-Groovy-RomanNumerals/src/solution.groovy
@@ -1,9 +1,17 @@
-/**
- * Created with IntelliJ IDEA.
- * User: shawn
- * Date: 8/14/13
- * Time: 12:16 AM
- * To change this template use File | Settings | File Templates.
- */
 class solution {
+    static void main (String[] args) {
+        def line1
+        def values = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+        def numerals = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
+        line1 = new File(args[0]).eachLine { line ->
+            def results = ""
+            for(int i = 0; i < 13; i ++) {
+                while ( line > values[i]) {
+                    line -= values[i];
+                    results += numerals[i];
+                }
+            }
+            System.out.println(results)
+        }
+    }
 }
