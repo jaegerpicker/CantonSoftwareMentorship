d2dbb092bd1eeb64eab6ed2ea8fbda2b28c68293
Shawn Campbell <jaegerpicker@gmail.com>
2013-08-17 23:47:33 -0400

    Updated the optimized c version.

--- 201309_RomanNumerals/ShawnCampbell-Month4-C-RomanNumerals/RomanNumerals/RomanNumerals/main.c
+++ 201309_RomanNumerals/ShawnCampbell-Month4-C-RomanNumerals/RomanNumerals/RomanNumerals/main.c
@@ -10,7 +10,27 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+#include "main.h"
 
+char * convert_num_to_roman(char *line, const char** numerals, int* values)
+{
+    int number = atoi(line);
+    char * res = malloc(20);
+    for(int i = 0; i < 13; i++) {
+        while(number > values[i]) {
+            //res = malloc(strlen(res)+strlen(numerals[i])+1);
+            number -= values[i];
+            strcat(res, numerals[i]);
+        }
+    }
+    return res;
+}
+
+/*void generate_lookuptab() {
+    for(int i = 0; i <= 3999; i++) {
+        tab[i] = {i, convert_num_to_roman(itoa(i), numerals, values)};
+    }
+}*/
 
 int main(int argc, const char * argv[])
 {
@@ -20,35 +40,29 @@ int main(int argc, const char * argv[])
     char * line = NULL;
     size_t len = 0;
     ssize_t read;
+    int path = 0;
     fp = fopen(argv[1], "r");
-    int values[13] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
-    const char *numerals[13];
-    numerals[0] = "M";
-    numerals[1] = "CM";
-    numerals[2] = "D";
-    numerals[3] = "CD";
-    numerals[4] = "C";
-    numerals[5] = "XC";
-    numerals[6] = "L";
-    numerals[7] = "XL";
-    numerals[8] = "X";
-    numerals[9] = "IX";
-    numerals[10] = "V";
-    numerals[11] = "IV";
-    numerals[12] = "I";
+    if(argc > 2) {
+        const char * compC = "c";
+        const char * compL = "l";
+        if(strncmp(argv[2], compC, sizeof(*argv[2])) == 0) { //straight calc
+            path = 0;
+        } else if(strncmp(argv[2], compL, sizeof(*argv[2])) == 0) { //lookup list
+            path = 1;
+        }
+    }
+    //generate_lookuptab();
     if(fp == NULL) {
         exit(EXIT_FAILURE);
     }
     int count = 0;
     while((read = getline(&line, &len, fp)) != -1) {
-        int number = atoi(line);
-        char * res = malloc(20);
-        for(int i = 0; i < 13; i++) {
-            while(number > values[i]) {
-                //res = malloc(strlen(res)+strlen(numerals[i])+1);
-                number -= values[i];
-                strcat(res, numerals[i]);
-            }
+        char *res = NULL;
+        if(path == 0) {
+            res = convert_num_to_roman(line, numerals, values);
+        } else if(path == 1) {
+            res = tab[atoi(line)].roman;
+            //printf("%i %s\n %")
         }
         printf("%i %s\n", count, res);
         count++;
